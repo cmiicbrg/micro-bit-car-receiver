4ea135ca7a0319bbf1a4bc2298c7307b955bef6b
Christoph Stadlbauer <christoph.stadlbauer@brgenns.ac.at>
2021-02-23 16:01:55 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,48 @@
+function vor () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+}
+function stop () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+}
+input.onButtonPressed(Button.A, function () {
+    vor()
+})
+function zurück () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+input.onButtonPressed(Button.AB, function () {
+    zurück()
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "vor") {
+        vor()
+    } else if (receivedString == "zurück") {
+        zurück()
+    } else if (receivedString == "stop") {
+        stop()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    stop()
+})
+stop()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
